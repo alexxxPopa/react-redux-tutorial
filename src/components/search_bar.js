81bc55f9e20fe2c77773c9b4c0ddb48fe0e7ecb7
alexxxPopa <alex.popa@mbitcasino.com>
2017-09-20 16:15:08 +0300

    Minor changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,20 +5,21 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: ''}
+    this.state = { term: ''};
   }
   render() {
-    return
+    return(
     <div> 
     <input
     value={this.state.term} 
     onChange={event => this.onInputChange(event.target.value)}/>
     </div>
+    );
   }
 
   onInputChange(term) {
     this.setState( {term} );
-    this.props.OnSearchTermChange(term);
+    this.props.onSearchTermChange(term);
   }
 }
 
